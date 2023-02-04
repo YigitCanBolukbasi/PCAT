d6da4c5d3470f5854ff90e7bb8e33d4868a90ea0
Yiğit Can bölükbaşı <91588692+YigitCanBolukbasi@users.noreply.github.com>
2023-02-04 13:30:31 +0300

    statik files are implemented

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const fn = function () {
-  console.log('function working');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 3000;
+
+const myLogger = (res, req, next) => {
+  console.log('middleware log 1');
+  next();
 };
+
+//middle ware
+app.use(express.static('public'));
+app.use(myLogger);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on ${port}`);
+});
